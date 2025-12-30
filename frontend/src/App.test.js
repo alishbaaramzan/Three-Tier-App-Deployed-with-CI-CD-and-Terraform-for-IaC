9ab3d507ad6b56cecb70a544b11e8d80d379afa3
alishbaaramzan <wizzz1211@gmail.com>
2025-12-31 01:00:54 +0500

    Fixed test issues

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Task Management App heading', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Task Management App/i);
   expect(linkElement).toBeInTheDocument();
 });
